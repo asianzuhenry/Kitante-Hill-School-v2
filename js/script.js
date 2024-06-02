489c01c83b2574922a1cff6a21c02844407d83ac
Asianzu Henry <asianzuhenry@gmail.com>
2024-06-02 17:02:19 +0300

    Adjusted the transition and rotation of icons and removed the console logs

--- js/script.js
+++ js/script.js
@@ -17,27 +17,20 @@ const Pages = [
     }
 ]
 
-
-
 const LI = document.querySelectorAll('li')
 const WRAPPER = document.querySelector('.wrapper section h3')
 
-console.log(WRAPPER.innerText);
-
 LI.forEach(Li => {
-    console.log(Li);
     Li.addEventListener('click', () => {
         Pages.forEach(page => {
 
             if (Li.classList = 'current') {
-                const LI = document.querySelectorAll('li')
                 LI.forEach(Li => {
                     Li.classList = ''
                 })
             }
             if (page.id === Li.id) {
                 WRAPPER.innerText = page.Text;
-                console.log(`page id matches li id, Pge id is ${page.id}`);
             }
             Li.classList = 'current'
 
